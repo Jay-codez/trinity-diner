2f2907db965acc99cf779b990e1823c6da29a4a7
Joseph <joseph.special1@gmail.com>
2021-10-12 02:19:10 -0400

    routing... ejs views

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const express = require("express");
+const path = require("path")
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
+
+
 
 const PORT = 3000;
 
+app.set("view engine","ejs")
+app.use("/",express.static(path.join(__dirname,"assets")))
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("index");
 });
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/menu", (req, res) => {
+  res.render("menu");
+});
+
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/fooditem/:id", (req, res) => {
+  
+
+  console.log(req.params)
+  
+  res.render("fooditem");
+
+
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running port ${PORT}`);
