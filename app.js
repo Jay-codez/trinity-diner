8bd76bb7f984aaa243620337845c4258567d84cb
Joseph <joseph.special1@gmail.com>
2021-10-14 06:44:06 -0400

    account creation attempts...orders without account(guests)

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const path = require("path")
 const connectDb = require("./db/connect-db")
+
+const Signin = require("./models/signin")
+const Account = require("./models/account")
+const Order = require("./models/order")
+
 const app = express();
 
 
-const contactRouter = require("./routes/contact")
+const contactRouter = require("./routes/contact");
+
+const { render } = require("ejs");
+
 const PORT = 3000;
+const start = async ()=>{
+  try {
+    await connectDb()
+    app.listen(PORT, () => {
+      console.log(`Server is running port ${PORT}`);
+    });
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+start()
 
 
 app.use(bodyParser.json());
@@ -16,19 +36,90 @@ app.use(
   })
 )
 app.set("view engine","ejs")
-app.use("/",express.static(path.join(__dirname,"assets")))
+app.use("/", express.static("assets"))
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About Us" });
 });
 
 app.get("/menu", (req, res) => {
-  res.render("menu");
+  res.render("menu", { title: "Menu" });
 });
+
+
+app.get("/accounts/signup", (req, res) => {
+  res.render("signup");
+});
+
+
+
+app.get("/signin", (req, res) => {
+  const signin = new Signin({
+    email: "new@email.com",
+    password: "password1"
+  });
+
+  signin.save()
+});
+
+
+
+app.post("/add-account", (req, res) => {
+  const account = new Account(req.body);
+
+  account.save()
+    .then((result) => {
+      res.render("/signin");
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+});
+
+//const acc_id = req.params.id;
+
+app.get("/find-account", (req, res) => {
+  const id = req.params.id;
+  Signup.findById(id)
+    .then(result => {
+      res.render("account", { signup: result, email: "new@email.com" });
+    })
+    .catch(err => {
+      console.log(err);
+  })
+})
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Signup.findById(id)
+    .then(result => {
+      res.render("account", { signup: result, email: "new@email.com" });
+    })
+    .catch(err => {
+      console.log(err);
+  })
+})
+
+
+app.get("/orders", (req, res) => {
+  Order.find()
+    .then((result) => {
+    res.render("index", {title: "Orders", orders: result})
+    })
+    .catch((err) => {
+    console.log(err)
+  })
+});
+
+app.get("/orders/create", (req, res) => {
+  res.render("orders", {title: "New order"});
+});
+
 
 
 app.get("/fooditem/:id", (req, res) => {
@@ -45,17 +136,4 @@ app.use(contactRouter)
 
 
 
-const start = async ()=>{
-    try {
-      await connectDb()
-      app.listen(PORT, () => {
-        console.log(`Server is running port ${PORT}`);
-      });
-      
-    } catch (error) {
-      console.log(error);
-    }
-}
 
-
-start()
