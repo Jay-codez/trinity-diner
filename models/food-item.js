f6df833fa88b0c90a5a9f8f2e9b422df45df13a0
Joseph <joseph.special1@gmail.com>
2021-11-09 01:37:39 -0500

    add items form fix... personalize...

--- models/food-item.js
+++ models/food-item.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 
 const foodItemSchema = new mongoose.Schema({
     name: String,
-    description: String,
-    price: Number,
+  price: Number,
+  description: String,
     featured:{
       type:  Boolean,
       default:false
